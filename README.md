c135e237c1cdee22aafa9d0d2fac9518f9dd1492
yangxin11 <yangxin11@lenovo.com>
2024-01-07 17:29:17 +0800

    client-side code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 第一步：grpc 的核心就是编写服务定义文件 *.proto
-    这个demo中是 hello.proto, .proto 文件使用 Protocol Buffers (ˈproʊtəkɑːl) 语法来定义服务和消息。这个文件定义了服务的接口和通信协议，以及客户端和服务器之间交换的数据结构。
+    我们可以在一个项目中编写多个 .proto 文件（便于多人协同开发）, 或者在 .proto 文件中定义多个 service 模块（便于阅读）
+    本demo中是 hello.proto, .proto 文件使用 Protocol Buffers (ˈproʊtəkɑːl) 语法来定义服务和消息。该文件定义了 服务的接口 和 通信协议，以及客户端和服务器之间交换的数据结构。
 
 第二步：服务端编写
     1）服务定义文件写好之后，使用命令生成相应的服务端 go 代码：
@@ -15,3 +16,11 @@
         启动服务，gRPC Server 开始 lis.Accept，直到Stop
 
 第三步：客户端编写
+    1）将服务定义文件hello.proto 也复制一份到客户端
+    2）生成客户端gRPC代码，和在服务端的操作命令一样
+        protoc --go_out=.  hello.proto
+        protoc --go-grpc_out=.  hello.proto
+    3）grpc.Dial 连接服务端地址，然后我们创建一个 gRPC客户端对象
+    4）使用创建好的客户端对象调用本地的方法，grpc 会将请求发送给服务端对应的方法，并返回响应结果
+
+
--- client/main.go
+++ client/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "demo.test/grpc-demo/client/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	// 连接到grpc server端，此处禁用安全传输，没有加密和验证
+	conn, err := grpc.Dial("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("grpc connection err: %v", err)
+	}
+	defer conn.Close()
+
+	// Create client
+	client := pb.NewSayHelloClient(conn)
+
+	// 通过本地调用来调用远程的方法（是不是很方便!）
+	resp, err := client.SayHello(context.Background(), &pb.HelloRequest{RequestName: "Zhang三", Age: 20})
+	if err != nil {
+		log.Printf("grpc client call failed, err: %v", err)
+	} else {
+		fmt.Println(resp.GetResponseMsg())
+	}
+
+}
